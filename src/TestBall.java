c526a212aa7ee9f1d70a61e4052851ddca7939fd
Fanotik <enotikfanotik@gmail.com>
2020-09-16 10:19:50 +0300

    Initial commit

---
+++ src/TestBall.java
@@ -0,0 +1,8 @@
+public class TestBall {
+    public static void main(String[] args) {
+        Ball b1 = new Ball(43324, 1243250);
+        System.out.println(b1);
+        b1.move(33, 23);
+        System.out.println(b1);
+    }
+}
